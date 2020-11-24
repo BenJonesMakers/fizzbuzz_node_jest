39c71f4d014502c21b2b15c431b6daa9f527586a
Ben Jones <benwjones76@outlook.com>
2020-11-24 11:25:08 +0000

    added if for fizz and buzz

--- fz.js
+++ fz.js
@@ -1,6 +1,10 @@
 class FizzBuzz {
   static method(num){
-    return "fizz";
+    if (num % 3 === 0) {
+      return "fizz"
+    } else if (num % 5 === 0) {
+      return "buzz"
+    }
   }
 }
 
--- fz.test.js
+++ fz.test.js
@@ -3,3 +3,7 @@ const FizzBuzz = require('./fz.js');
 test("returns 'fizz' if divisable by 3", function(){
   expect(FizzBuzz.method(3)).toBe("fizz");
 })
+
+test("returns 'buzz' if divisible by 5", function() {
+  expect(FizzBuzz.method(5)).toBe("buzz");
+})
